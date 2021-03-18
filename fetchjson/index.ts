fbfcde35fd395045ff5e05d4a8ea781bd01ca76a
Arshi Bhasin <arshi_b@hotmail.com>
2021-03-18 09:10:43 -0400

    fetchjson

---
+++ fetchjson/index.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'; // module used for network requests
+
+const url = 'http://jsonplaceholder.typicode.com/todos/1';
+
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then(response => {
+    const todo = response.data as Todo;
+
+    const ID = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    
+    console.log(`
+        The Todo with ID: ${ID}
+        Has a title of : ${title}
+        Is it finished? ${completed}
+    `)
+})
+
+
+
+
