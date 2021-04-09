c847d47692c2b5f699c58e107a93fa165d5d20bd
Arshi Bhasin <arshi_b@hotmail.com>
2021-04-09 06:37:48 -0400

    intro to types

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,28 +1,40 @@
-import axios from 'axios'; // module used for network requests
+import axios from "axios"; // module used for network requests
 
-const url = 'http://jsonplaceholder.typicode.com/todos/1';
-
+const url = "http://jsonplaceholder.typicode.com/todos/1";
 
 interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
+  // This interface defines a new Type called 'Todo'
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-axios.get(url).then(response => {
-    const todo = response.data as Todo;
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
 
-    const ID = todo.id;
-    const title = todo.title;
-    const completed = todo.completed;
-    
-    console.log(`
-        The Todo with ID: ${ID}
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(ID, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+        The Todo with ID: ${id}
         Has a title of : ${title}
         Is it finished? ${completed}
-    `)
-})
+    `);
+};
 
-
-
-
+// Notes:
+// ======
+// Every value in TS has a Type
+// 1. Primitive types are self explanatory
+// 2. Object Types are functions, arrays, classes, objects
+//    (any types we create)
+//
+// Why do we care about types?
+// 1. Types are used to analyze our code for errors
+// 2. Allows other engineers to understand what values are
+//    flowing around in our codebase
