6e620afa50aee537c7a9380e938464ca50ddd481
Arshi Bhasin <arshi_b@hotmail.com>
2021-04-10 19:08:30 -0400

    removed some type annotations

--- fetchjson/features/annotations/variables.ts
+++ fetchjson/features/annotations/variables.ts
@@ -1,13 +1,13 @@
 // primitive types
-let apples: number = 10; // example of type annotation
-let speed: string = 'fast';
-let nothing_much: null = null;
-let nothing: undefined = null; // explained below
+let apples = 10; // example of type annotation
+let speed = 'fast';
+let nothing_much = null;
+let nothingned = null; // explained below
 // null !== undefined # strictly unequivalent
 // null == undefined # loosely equivalent
 
 // built in objects
-let now: Date = new Date();
+let now = new Date();
 
 // Array
 let colors: string[] = ['red', 'green', 'blue']
@@ -26,10 +26,14 @@ let point: { x: number; y: number; z: number } = {
     z: 10
 }
 
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+}
 
 
-
-
-
+// Summary
+// All the above are written in type annotations,
+// but not even a single example above requires type annotations
 
 // What is the point of a type annotation? 
